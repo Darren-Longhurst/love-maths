a8a99f59e161fb56575765da227daacb5008ba4c
Darren-Longhurst <dmlonghurst@icloud.com>
2025-08-30 15:48:21 +0100

    Set up strcuture of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+function runGame()  {
+
+}
+
+function checkAnswer()  {
+
+}
+
+function calculateCorrctAnswer()  {
+
+}
+
+function incrementScore()  {
+
+}
+
+function incrementWrongAnswer()  {
+
+} 
+
+function displayAdditionQuestion()  {
+
+}
+
+function displaySubtractQuestion()  {
+
+}
+
+function displaySubtractQuestion()  {
+
+}
+
+function displayMultiplyQuestion()  {
+
+}
+
