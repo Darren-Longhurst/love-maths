3900cac332a7dbde581d47a5ba47e4e3d46938f0
Darren-Longhurst <dmlonghurst@icloud.com>
2025-08-31 10:45:51 +0100

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,9 @@ function runGame(gameType)  {
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition")  {
-        displayAdditionQuestion(num1, num2);  
+        displayAdditionQuestion(num1, num2); 
+    } else if (gameType === "multiply")  {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -73,6 +75,8 @@ let operator = document.getElementById('operator').innerText;
 
 if (operator === "+")  {
     return [operand1 + operand2, "addition"];
+} else if (operator === "x")  {
+    return [operand1 * operand2, "multiply"];
 } else {
 alert(`Unimplemented operator ${operator}`);
 throw `Unimplemented operator ${operator}. Aborting!`;
@@ -115,7 +119,11 @@ function displaySubtractQuestion()  {
 
 }
 
-function displayMultiplyQuestion()  {
+function displayMultiplyQuestion(operand1, operand2)  {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
 
